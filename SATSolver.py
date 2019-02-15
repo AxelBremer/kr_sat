600c7c983b71bb218ee3ffa1761b4953fc3aced7
Axel <axelbremer94@gmail.com>
2019-02-15 10:52:28 +0100

    Finds tautologies TODO: remove satisfied clause

--- SATSolver.py
+++ SATSolver.py
@@ -1,6 +1,7 @@
 clauses = []
 literals = {}
 clause_counter = 0
+indices = []
 
 def add_to_lit_dict(lit, index):
     global literals
@@ -15,18 +16,18 @@ def add_to_lit_dict(lit, index):
     literals[lit].append((index, sign))
 
 def add_clauses(dimacs):
-    global clause_counter, clauses
+    global clause_counter, clauses, indices
     
-    print(clause_counter)
-    
-    for line in dimacs.split('\n')[1:]:
+    for line in dimacs.split('\n'):
         clause = []
         for lit in line.split():
+            if lit == 'p': break
             if lit != '0':
                 clause.append(lit)
                 add_to_lit_dict(lit, clause_counter)
             else:
                 clauses.append(clause)
+                indices.append(clause_counter)
                 clause_counter += 1
 
 def to_dimacs(sudokus):
@@ -42,10 +43,36 @@ def to_dimacs(sudokus):
                 ct += 1
         sudokus.append(sud)
     return sudokus
+    
+def satisfied(ind):
+    global indices
+    
+    indices.remove(ind)
+    for lit in literals:
+        for tup in literals[lit]:
+            if tup[0] == ind:
+                print(tup)
+                literals[lit].remove(tup)
+
+def find_tautologies():
+    prev_ind = -1
+    prev_signs = []
+    for lit in literals:
+        for (ind, sign) in literals[lit]:
+            if ind != prev_ind:
+                prev_signs = [sign]
+                prev_ind = ind
+            elif sign*-1 in prev_signs:
+                print('sat', ind)
+                print(clauses[ind])
+                satisfied(ind)
+            else:
+                prev_signs.append(sign)
+            #joe = raw_input('...')
 
 with  open("sudoku-rules.txt") as file:
     data = file.read()
-    
+
 add_clauses(data)
 
 with open("test sudokus/1000 sudokus.txt") as file:
@@ -56,3 +83,11 @@ sudokus = to_dimacs(data)
 sudoku_nr = 0
 
 add_clauses(sudokus[sudoku_nr])
+
+add_clauses("1811 -1811 0\n")
+
+print(literals['1811'])
+
+find_tautologies()
+
+print(literals['1811'])
